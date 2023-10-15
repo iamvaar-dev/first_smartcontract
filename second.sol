30a6250e98c6caea95707cbae2742295dddeb50b
Iamvaar <venkibvb5192@gmail.com>
2023-10-16 00:02:38 +0530

    second sol deleted

--- second.sol
+++
@@ -1,7 +0,0 @@
-    // SPDX-License-Identifier: MIT
-    pragma solidity >=0.7.0 <0.9.0;
-
-    contract demo{
-        string public str = "Hey, I am Venkatesh this is my second smart contract";
-
-    } 
