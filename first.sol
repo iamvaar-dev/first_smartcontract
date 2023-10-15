22699e7525439f47f157647a5a3381a3c2bdc510
Iamvaar <venkibvb5192@gmail.com>
2023-10-15 23:30:32 +0530

    This is my first smart contract

---
+++ first.sol
@@ -0,0 +1,7 @@
+    // SPDX-License-Identifier: MIT
+    pragma solidity >=0.7.0 <0.9.0;
+
+    contract demo{
+        string public str = "Hey, I am Venkatesh this is my first smart contract";
+        
+    }
